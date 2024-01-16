1945a10d8bb8deee75a9f97877ee82ce712feb0e
Niket Yende <niket6@gmail.com>
2024-01-16 18:51:17 +0530

    Fix issues with solquad test suite

--- tests/solquad.ts
+++ tests/solquad.ts
@@ -13,8 +13,7 @@ describe("solquad", async () => {
   const connection = new anchor.web3.Connection('http://127.0.0.1:8899', 'confirmed');
   // const programId = new anchor.web3.PublicKey("3fowu869PY6frqrYPdhtCzsm7j1jgjpr47HyuyMP9xUH");
   const programId = new anchor.web3.PublicKey("3sd5iCCNUngPFu9QwgeTACP9KiiPaCt1bfBbsCjL64u3");
-  // const programId = "3sd5iCCNUngPFu9QwgeTACP9KiiPaCt1bfBbsCjL64u3";
-
+  
   const admin1 = anchor.web3.Keypair.generate();
   const admin2 = anchor.web3.Keypair.generate();
   const wallet1 = new anchor.Wallet(admin1);
@@ -36,49 +35,56 @@ describe("solquad", async () => {
   // const voter5 = anchor.web3.Keypair.generate();
   // const voter6 = anchor.web3.Keypair.generate();
 
-  const [escrowPDA] = anchor.web3.PublicKey.findProgramAddressSync([
-    utf8.encode("escrow"),
-    admin1.publicKey.toBuffer(),
-  ],
-    program1.programId
-  );
+  let escrowPDA, poolPDA, projectPDA1, differentEscrowPDA, differentPoolPDA;
 
-  const [poolPDA] = anchor.web3.PublicKey.findProgramAddressSync([
-    utf8.encode("pool"),
-    admin1.publicKey.toBuffer(),
-  ],
-    program1.programId
-  );
+  before(async () => {
+    [escrowPDA] = anchor.web3.PublicKey.findProgramAddressSync([
+      utf8.encode("escrow"),
+      admin1.publicKey.toBuffer(),
+    ],
+      program1.programId
+    );
+  
+    [poolPDA] = anchor.web3.PublicKey.findProgramAddressSync([
+      utf8.encode("pool"),
+      admin1.publicKey.toBuffer(),
+    ],
+      program1.programId
+    );
+  
+    [projectPDA1] = anchor.web3.PublicKey.findProgramAddressSync([
+      utf8.encode("project"),
+      poolPDA.toBytes(),
+      admin1.publicKey.toBuffer(),
+    ],
+      program1.programId
+    );
+  
+    [differentEscrowPDA] = anchor.web3.PublicKey.findProgramAddressSync([
+      utf8.encode("escrow"),
+      admin2.publicKey.toBuffer(),
+    ],
+      program1.programId
+    );
+  
+    [differentPoolPDA] = anchor.web3.PublicKey.findProgramAddressSync([
+      utf8.encode("pool"),
+      admin2.publicKey.toBuffer()
+    ],
+      program1.programId
+    );
 
-  const [projectPDA1] = anchor.web3.PublicKey.findProgramAddressSync([
-    utf8.encode("project"),
-    poolPDA.toBytes(),
-    admin1.publicKey.toBuffer(),
-  ],
-    program1.programId
-  );
+    await airdrop(admin1, provider1);
+    await airdrop(admin2, provider1);
 
-  const [differentEscrowPDA] = anchor.web3.PublicKey.findProgramAddressSync([
-    utf8.encode("escrow"),
-    admin2.publicKey.toBuffer(),
-  ],
-    program1.programId
-  );
-
-  const [differentPoolPDA] = anchor.web3.PublicKey.findProgramAddressSync([
-    utf8.encode("pool"),
-    admin2.publicKey.toBuffer()
-  ],
-    program1.programId
-  );
-
-  // airdrop(admin1, provider1);
-  // airdrop(admin2, provider1);
+    
+  });
+ 
 
   // Test 1
   it("initializes escrow and pool", async () => {
-    await airdrop(admin1, provider1);
-    await airdrop(admin2, provider1);
+    // await airdrop(admin1, provider1);
+    // await airdrop(admin2, provider1);
     const poolIx = await program1.methods.initializePool().accounts({
       poolAccount: poolPDA,
     }).instruction();
@@ -125,49 +131,82 @@ describe("solquad", async () => {
 
   // Test 3
   it("tries to add the project in the different pool", async() => {
-    const poolIx = await program2.methods.initializePool().accounts({
-      poolAccount: differentPoolPDA,
-    }).instruction();
+    try {
+      const poolIx = await program2.methods.initializePool().accounts({
+        poolAccount: differentPoolPDA,
+      }).instruction();
+  
+      const escrowIx = await program2.methods.initializeEscrow(new BN(10000)).accounts({
+        escrowAccount: differentEscrowPDA,
+      })
+      // .instruction();
+      .postInstructions([poolIx])
+      .rpc();
+  
+      console.log("New Escrow and different Pool are successfully created!", escrowIx);
+  
+      const addProjectTx = await program2.methods.addProjectToPool().accounts({
+        projectAccount: projectPDA1,
+        poolAccount: differentPoolPDA,
+        escrowAccount: differentEscrowPDA
+      })
+      .instruction();
+  
+      const initProjectTx = await program2.methods.initializeProject("My Project").accounts({
+        projectAccount: projectPDA1,
+        poolAccount: differentPoolPDA,
+      })
+      .postInstructions([addProjectTx])
+      .rpc();
+  
+      console.log("Project successfully initialized and added for the differentPoolPDA", initProjectTx);
 
-    const escrowIx = await program2.methods.initializeEscrow(new BN(10000)).accounts({
-      escrowAccount: differentEscrowPDA,
-    })
-    .instruction()
-
-    const addProjectTx = await program2.methods.addProjectToPool().accounts({
-      projectAccount: projectPDA1,
-      poolAccount: differentPoolPDA,
-      escrowAccount: differentEscrowPDA
-    })
-    .preInstructions([escrowIx, poolIx])
-    .rpc();
-
-    console.log("Different pool is created and the project is inserted into it", addProjectTx);
-
-    const data = await program1.account.pool.fetch(differentPoolPDA)
-    console.log("data projects", data.projects);
+      const data = await program1.account.pool.fetch(differentPoolPDA)
+      console.log("data projects", data.projects);
+    } catch(_err) {
+      console.log('Error caught: Seed constraint error');
+      expect(_err).to.be.instanceOf(AnchorError);
+      const err: AnchorError = _err;
+      expect(err.error.errorCode.number).to.equal(2006);
+      expect(err.error.errorMessage).to.equal('A seeds constraint was violated');
+    }
   });
 
   // Test 4
   it("votes for the project and distributes the rewards", async() => {
-    const distribIx = await program1.methods.distributeEscrowAmount().accounts({
-      escrowAccount: escrowPDA,
-      poolAccount: poolPDA,
-      projectAccount: projectPDA1,
-    })
-    .instruction();
-
-    const voteTx = await program1.methods.voteForProject(new BN(10)).accounts({
-      poolAccount: poolPDA,
-      projectAccount: projectPDA1,
-    })
-    .postInstructions([distribIx])
-    .rpc();
-    
-    console.log("Successfully voted on the project and distributed weighted rewards", voteTx);
-
-    const ant = await program1.account.project.fetch(projectPDA1)
-    console.log("amount", ant.distributedAmt.toString());
+    try {
+      await program1.methods.initializeProject("My Project").accounts({
+        projectAccount: projectPDA1,
+        poolAccount: poolPDA
+      })
+      .postInstructions([])
+      .rpc();
+  
+      const distribIx = await program1.methods.distributeEscrowAmount().accounts({
+        escrowAccount: escrowPDA,
+        poolAccount: poolPDA,
+        projectAccount: projectPDA1,
+      })
+      .instruction();
+  
+      const voteTx = await program1.methods.voteForProject(new BN(10)).accounts({
+        poolAccount: poolPDA,
+        projectAccount: projectPDA1,
+      })
+      .postInstructions([distribIx])
+      .rpc();
+      
+      console.log("Successfully voted on the project and distributed weighted rewards", voteTx);
+  
+      const ant = await program1.account.project.fetch(projectPDA1)
+      console.log("amount", ant.distributedAmt.toString());
+    } catch(_err) {
+      console.log('Error caught: Seed constraint error');
+      expect(_err).to.be.instanceOf(AnchorError);
+      const err: AnchorError = _err;
+      console.log(err.error.errorCode.number);
+      console.log(err.error.errorMessage);
+    }
   });
 });
 
